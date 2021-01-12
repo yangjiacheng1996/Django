851e5253766eaf29be21eababde7066d0ed505d6
yangjiacheng <2290906844@qq.com>
2021-01-13 01:47:56 +0800

    model

--- HelloDjango/Two/urls.py
+++ HelloDjango/Two/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from Two import views
 
 urlpatterns = [
-    url(r'^index/',views.index)
+    url(r'^index/',views.index),
+    url(r'addstudent/',views.add_student),
+    url(r'getstudents/',views.get_students)
 ]
--- HelloDjango/Two/views.py
+++ HelloDjango/Two/views.py
@@ -1,8 +1,27 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from Two.models import Student
 
 # Create your views here.
 
 def index(requests):
     return HttpResponse('Two index')
+
+def add_student(requests):
+    student = Student()
+    student.s_name = 'Jenny'
+    student.s_age = 27
+    student.save()
+    return HttpResponse('Add Succes : %s.'%student.s_name)
+
+def get_students(requests):
+    students = Student.objects.all()
+    for student in students:
+        print(student.s_name)
+    #return HttpResponse('Student List')
+    return render(requests,"student_list.html")
+
+
+
+
 
